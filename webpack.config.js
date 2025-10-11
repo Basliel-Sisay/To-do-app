48a306e5ca6511ff12868902f083b31ac71117ee
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-11 13:34:24 +0300

    Save current work before revert

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const html = require('html-webpack-plugin');
+
+module.exports={
+    mode :'development',
+    entry:'./src/index.js',
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'eval-source-map',
+    devServer:{
+        watchFiles:['./src/temp.html'],
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                use:['style-loader', 'css-loader'],
+            },
+            {
+                test:/\.html$/i,
+                loader: html-loader,
+            },
+            {
+                test:/\.(png|svg|jpeg|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins:[
+        new html({
+            template: './src/temp.html',
+        }),
+    ],
+
+};
