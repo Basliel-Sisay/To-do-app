fffffa5bdc06a4786546a501b98083b3c681f257
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-13 18:08:47 +0300

    Added the To-do-app controller for organizing the projects

---
+++ src/controller.js
@@ -0,0 +1,24 @@
+export class controller{
+    constructor(){
+        this.projects = [];
+        this.active = null;
+    }
+    create(project){
+        this.projects.push(project);
+        if(!this.active){
+            this.active = project;
+        }
+    }
+    remove(project){
+       const proj = this.projects.findIndex(list => list.id === project);
+       if(proj !==-1){
+        if(this.active && this.active.id === project){
+            this.active = null;
+        }
+        this.projects.splice(proj,1);
+       }
+    }
+    check(project){
+        this.active = this.projects.find(list =>list.id ===project);
+    }
+}
