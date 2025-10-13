3203ce4b401c937002d6d2692c15530861e095df
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-13 18:09:23 +0300

    Imported the controller.js and tested it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './style.css';
 import {Todo} from './todo.js';
 import {project} from './projects.js';
+import { controller } from './controller.js';
+const cont = new controller();
 const test = new project ('Testing my projects.js code');
 const check = new Todo ('test', 'testing the todo', '2025', 'high');
 console.log(check);
 console.log(test);
+console.log(cont);
