2484fb4b289f1814bb404fcda8877f89e358928c
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-13 17:47:04 +0300

    Imported the projects.js file and run a simple test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './style.css';
 import {Todo} from './todo.js';
+import {project} from './projects.js';
+const test = new project ('Testing my projects.js code');
 const check = new Todo ('test', 'testing the todo', '2025', 'high');
 console.log(check);
+console.log(test);
