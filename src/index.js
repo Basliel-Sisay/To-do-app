8f15e2343c1b8b5b157fc5b1242879fb29338eb9
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-14 17:55:31 +0300

    tested the dom.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import {Todo} from './todo.js';
 import {project} from './projects.js';
 import { controller } from './controller.js';
 import {dom} from './dom.js';
-const cont = new controller();
-const test = new project ('Testing my projects.js code');
-const check = new Todo ('test', 'testing the todo', '2025', 'high');
-const doms = new dom(cont);
-doms.listener();
-console.log(check);
-console.log(test);
-console.log(cont);
-console.log(doms);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cont = new controller();
+    const test = new project ('Testing my projects.js code');
+    const check = new Todo ('test', 'testing the todo', '2025', 'high');
+    const doms = new dom(cont);
+    doms.listener();
+    console.log(check);
+    console.log(test);
+    console.log(cont);
+    console.log(doms);
+}); 
