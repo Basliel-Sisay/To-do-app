2efe6aefc2686c7efc9e991990315215d90d2def
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-14 17:54:49 +0300

    made small adjustment while debugging

--- src/controller.js
+++ src/controller.js
@@ -9,16 +9,8 @@ export class controller{
             this.active = project;
         }
     }
-    remove(project){
-       const proj = this.projects.findIndex(list => list.id === project);
-       if(proj !==-1){
-        if(this.active && this.active.id === project){
-            this.active = null;
-        }
-        this.projects.splice(proj,1);
-       }
-    }
     check(project){
-        this.active = this.projects.find(list =>list.id ===project);
+        this.active = this.projects.find((list) =>list.id ===project);
     }
 } 
+    
--- src/projects.js
+++ src/projects.js
@@ -8,12 +8,6 @@ export class project{
         this.todoList.push(todo);
     }
     remove(id){
-        const ids = this.todoList.findIndex(todo=> todo.id === id);
-        if (ids !== -1){
-            this.todoList.splice(ids, 1);
-        }
-        else{
-            throw new Error('There is no match with this ID');
-        }
+        this.todoList= this.todoList.filter((t)=> t.id !== id);
     }
 }
