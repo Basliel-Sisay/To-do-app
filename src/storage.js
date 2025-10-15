391c1ee23c5e75b6190c5fa9bf69fef955b57265
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-15 10:17:21 +0300

    The storage file with save method that converts to controller into json

---
+++ src/storage.js
@@ -0,0 +1,18 @@
+import { controller } from "./controller";
+
+export class storage{
+   save(controller){
+    let activeId;
+    if(controller.active){
+        activeId= controller.active.id;
+    }
+    else{
+        activeId= null;
+    };
+    const store ={
+        projects : controller.projects,
+        activeProject: activeId
+    };
+    localStorage.setItem('ToDoAppStore' , JSON.stringify(store));
+   }
+}
